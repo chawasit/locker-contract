d1396fb2a39e00be1b78a585cff9dcab84f0eb38
Chawasit Tengtrairatana <chawasit555@gmail.com>
2018-11-30 04:22:42 +0700

    added ropsten network via infura api

--- truffle.js
+++ truffle.js
@@ -1,3 +1,8 @@
+require('dotenv').config()
+
+var HDWalletProvider = require("truffle-hdwallet-provider");
+
+
 module.exports = {
   // See <http://truffleframework.com/docs/advanced/configuration>
   // for more about customizing your Truffle configuration!
@@ -6,12 +11,20 @@ module.exports = {
       host: "127.0.0.1",
       port: 7545,
       network_id: "*" // Match any network id
-    }, 
+    },
     live: {
       host: "127.0.0.1",
       port: 8545,
-      network_id: "1", // main ethereum
-      from: "0x3b20c0a9c1bf9828c7d50d6355ee5AD8B037b015"
+      network_id: "1" // public ethereum
+    },
+    ropsten: {
+      provider: () => new HDWalletProvider(
+        process.env.MNEMONIC,
+        "https://ropsten.infura.io/v3/" + process.env.INFURA_API_KEY,
+        1
+      ),
+      network_id: 3,
+      gas: 4700000
     }
   },
   mocha: {
